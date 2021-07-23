10690259d59e9f412b6b1b2f621fbb3eb06aa506
sandeepb20 <sandeepskr1721@gmail.com>
2021-07-23 23:02:09 +0530

    bot

---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    UltraPureWords = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bsdk',
+        'bc',
+        'mc',
+        'You suck men',
+        
+    ]
+
+    if message.content:
+        response = random.choice(UltraPureWords)
+        await message.channel.send(response)
+
+client.run(TOKEN)
